d579e2d6afee88673880773b19ea5ee990e3b923
Wooki <wooki1@op.pl>
2018-07-21 22:07:20 +0200

    Animations for girls

--- Assets/Scripts/Girl.cs
+++ Assets/Scripts/Girl.cs
@@ -1,39 +1,92 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 
 public class Girl : MonoBehaviour
 {
-    [SerializeField]
-    public float WOO_LIMIT = 1.0f;
-    [SerializeField]
-    public float WOO_DECAY = 0.1f;
-    public const int NOT_TAKEN = -1;
+    public RhythmSpriteSequence unimpressed;
+    public RhythmSpriteSequence idle;
+    public RhythmSpriteSequence happy;
+    public RhythmSpriteSequence excited;
+    public RhythmSpriteSequence slap;
+    private RhythmSpriteAnimation rhythmSpriteAnimation;
 
+    [SerializeField]
+    public float WOO_TOP_LIMIT = 1.0f;
+    [SerializeField]
+    public float WOO_EXCITED_LIMIT = 0.8f;
+    private const int WOO_EXCITED_INDEX = 3;
+    [SerializeField]
+    public float WOO_HAPPY_LIMIT = 0.4f;
+    private const int WOO_HAPPY_INDEX = 2;
+    [SerializeField]
+    public float WOO_IDLE_LIMIT = 0.0f;
+    private const int WOO_IDLE_INDEX = 1;
+    [SerializeField]
+    public float WOO_BOTTOM_LIMIT = -0.2f;
+    private const int WOO_UNIMPRESSED_INDEX = 0;
+    [SerializeField]
+    public float WOO_DECAY = 0.01f;
+    [SerializeField]
+    public float SLAP_COOLDOWN = 1.0f;
+    [SerializeField]
+    public float REACTION_RANGE = 10.0f;
+
+    public const int NOT_TAKEN = -1;
     private int takenBy = NOT_TAKEN;
+    private const int SLAP_ANIMATION_INDEX = -1;
+    private float slapCooldown = 0.0f;
     Dictionary<int, float> currentWooFactors = new Dictionary<int, float>();
+    KeyValuePair<float, RhythmSpriteSequence>[] animationChangeBorders;
+    private int currentAnimationIndex = SLAP_ANIMATION_INDEX;
+
+    private void Awake()
+    {
+        rhythmSpriteAnimation = GetComponentInChildren<RhythmSpriteAnimation>();
+        Assert.IsNotNull(rhythmSpriteAnimation, "Missing rhythm sprite animation");
+        Assert.IsNotNull(unimpressed, "Missing unimpressed rhythm sprite sequence");
+        Assert.IsNotNull(idle, "Missing idle rhythm sprite sequence");
+        Assert.IsNotNull(happy, "Missing happy rhythm sprite sequence");
+        Assert.IsNotNull(excited, "Missing excited rhythm sprite sequence");
+        Assert.IsNotNull(slap, "Missing slap rhythm sprite sequence");
+        animationChangeBorders = new KeyValuePair<float, RhythmSpriteSequence>[4];
+        animationChangeBorders[WOO_UNIMPRESSED_INDEX] = new KeyValuePair<float, RhythmSpriteSequence>(WOO_BOTTOM_LIMIT, null);
+        animationChangeBorders[WOO_IDLE_INDEX] = new KeyValuePair<float, RhythmSpriteSequence>(WOO_IDLE_LIMIT, null);
+        animationChangeBorders[WOO_HAPPY_INDEX] = new KeyValuePair<float, RhythmSpriteSequence>(WOO_HAPPY_LIMIT, null);
+        animationChangeBorders[WOO_EXCITED_INDEX] = new KeyValuePair<float, RhythmSpriteSequence>(WOO_EXCITED_LIMIT, null);
+    }
 
     private void Update()
     {
-        if (takenBy == NOT_TAKEN)
+        if (slapCooldown > 0.0f)
         {
-            //TODO: gamestate receiver
-            for (int i = 1; i < 10; ++i)
+            slapCooldown -= Time.deltaTime;
+        }
+        //TODO: gamestate receiver
+        for (int i = 1; i < 10; ++i)
+        {
+            if (currentWooFactors.ContainsKey(i) && i != takenBy)
             {
-                if(currentWooFactors.ContainsKey(i))
+                if (currentWooFactors[i] > WOO_IDLE_LIMIT)
                 {
-                    if (currentWooFactors[i] > 0.0f)
+                    currentWooFactors[i] -= Time.deltaTime * WOO_DECAY;
+                    if (currentWooFactors[i] < WOO_IDLE_LIMIT)
                     {
-                        currentWooFactors[i] -= Time.deltaTime * WOO_DECAY;
-                        if (currentWooFactors[i] < 0.0f)
-                        {
-                            currentWooFactors[i] = 0.0f;
-                        }
+                        currentWooFactors[i] = WOO_IDLE_LIMIT;
+                    }
+                }
+                else if (currentWooFactors[i] < WOO_IDLE_LIMIT)
+                {
+                    currentWooFactors[i] += Time.deltaTime * WOO_DECAY;
+                    if (currentWooFactors[i] > WOO_IDLE_LIMIT)
+                    {
+                        currentWooFactors[i] = WOO_IDLE_LIMIT;
                     }
                 }
             }
         }
-        //TODO: animacja
+        DetermineAnimation();
     }
 
     public void Woo(int playerNumber, float wooFactor)
@@ -47,14 +100,15 @@ public class Girl : MonoBehaviour
             else
             {
                 currentWooFactors.Add(playerNumber, wooFactor);
+                currentWooFactors[playerNumber] += WOO_IDLE_LIMIT;
             }
-            if (wooFactor > 0 && currentWooFactors[playerNumber] >= WOO_LIMIT)
+            if (wooFactor > 0 && currentWooFactors[playerNumber] >= WOO_TOP_LIMIT)
             {
                 takenBy = playerNumber;
             }
-            else if (wooFactor < 0 && currentWooFactors[playerNumber] < 0.0f)
+            else if (wooFactor < 0 && currentWooFactors[playerNumber] < WOO_BOTTOM_LIMIT)
             {
-                currentWooFactors[playerNumber] = 0.0f;
+                currentWooFactors[playerNumber] = WOO_BOTTOM_LIMIT;
             }
         }
     }
@@ -63,12 +117,80 @@ public class Girl : MonoBehaviour
     {
         if (currentWooFactors.ContainsKey(playerNumber))
         {
-            currentWooFactors[playerNumber] = 0.0f;
+            currentWooFactors[playerNumber] = WOO_BOTTOM_LIMIT;
             if(takenBy == playerNumber)
             {
                 takenBy = NOT_TAKEN;
             }
         }
-        //TODO: animacja plaskacza
+        slapCooldown = SLAP_COOLDOWN;
+    }
+
+    private void DetermineAnimation()
+    {
+        int newAnimationIndex = 0;
+        KeyValuePair<int, float> closestDancerData = new KeyValuePair<int, float>(-1, 10000.0f);
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, REACTION_RANGE);
+        foreach (var collider in collidersInRange)
+        {
+            Dancer dancer = collider.gameObject.GetComponent<Dancer>();
+            if (dancer != null)
+            {
+                if(closestDancerData.Key == -1 || Vector3.Distance(gameObject.transform.position, dancer.transform.position) < closestDancerData.Value)
+                {
+                    closestDancerData = new KeyValuePair<int, float>(dancer.PlayerNumber, Vector3.Distance(gameObject.transform.position, dancer.transform.position));
+                }
+            }
+        }
+        if(!currentWooFactors.ContainsKey(closestDancerData.Key))
+        {
+            currentWooFactors.Add(closestDancerData.Key, WOO_IDLE_LIMIT);
+        }
+        if(slapCooldown > 0.0f)
+        {
+            newAnimationIndex = SLAP_ANIMATION_INDEX;
+        }
+        else if(currentWooFactors[closestDancerData.Key] >= WOO_EXCITED_LIMIT)
+        {
+            newAnimationIndex = WOO_EXCITED_INDEX;
+        }
+        else if (currentWooFactors[closestDancerData.Key] >= WOO_HAPPY_LIMIT)
+        {
+            newAnimationIndex = WOO_HAPPY_INDEX;
+        }
+        else if (currentWooFactors[closestDancerData.Key] >= WOO_IDLE_LIMIT)
+        {
+            newAnimationIndex = WOO_IDLE_INDEX;
+        }
+        else
+        {
+            newAnimationIndex = WOO_UNIMPRESSED_INDEX;
+        }
+        if (newAnimationIndex != currentAnimationIndex)
+        {
+            currentAnimationIndex = newAnimationIndex;
+            switch(currentAnimationIndex)
+            {
+                case SLAP_ANIMATION_INDEX:
+                    rhythmSpriteAnimation.SpriteSequence = slap;
+                break;
+                    
+                case WOO_EXCITED_INDEX:
+                    rhythmSpriteAnimation.SpriteSequence = excited;
+                    break;
+                    
+                case WOO_HAPPY_INDEX:
+                    rhythmSpriteAnimation.SpriteSequence = happy;
+                    break;
+
+                case WOO_IDLE_INDEX:
+                    rhythmSpriteAnimation.SpriteSequence = idle;
+                    break;
+                    
+                case WOO_UNIMPRESSED_INDEX:
+                    rhythmSpriteAnimation.SpriteSequence = unimpressed;
+                break;
+            }
+        }
     }
 }

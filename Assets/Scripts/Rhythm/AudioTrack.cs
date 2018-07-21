e3e654db8cf2b8def04ea7428a473877ce31bede
Rafał Tyl <rafal.tyl@silverbullet.pl>
2018-07-21 12:49:35 +0200

    Start implementing rhythm classes, add missing .meta files

--- Assets/Scripts/Rhythm/AudioTrack.cs
+++ Assets/Scripts/Rhythm/AudioTrack.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+[System.Serializable]
+public class RhythmDataChangeMarker
+{
+    public int measure;
+    public RhythmData rhythmData;
+}
 
 public class AudioTrack : ScriptableObject
 {
     [SerializeField]
-    RhythmData rhythmData;
+    List<RhythmDataChangeMarker> rhythmChangeData;
 
     [SerializeField]
     AudioClip audioClip;
+
+    public RhythmData GetRhythmDataForMeasure(int measure)
+    {
+        var changeMarker = rhythmChangeData.FindLast((RhythmDataChangeMarker marker) => { return marker.measure <= measure; });
+        if (changeMarker == null)
+        {
+            return null;
+        }
+        return changeMarker.rhythmData;
+    }   
 }
--- Assets/Scripts/Rhythm/RhythmData.cs
+++ Assets/Scripts/Rhythm/RhythmData.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 using System.Collections.Generic;
 
 public class RhythmData : ScriptableObject
 {
+    float beatsPerMinute = 150;
+    float intensity = 0.5f;
+
+    [SerializeField]
+    bool[] measureAccentDistribution;
+
+    public float BeatsPerMinute { get => beatsPerMinute; set => beatsPerMinute = value; }
+    public float Intensity { get => intensity; set => intensity = value; }
+    public int TimeSignature { get => MeasureAccentDistribution.Length; }
+    public bool[] MeasureAccentDistribution { get => measureAccentDistribution; }
+  
+    public bool IsAccentOnBeat(int beat)
+    {
+        Assert.IsTrue(beat < MeasureAccentDistribution.Length);
+        return MeasureAccentDistribution[beat];
+    }
 }

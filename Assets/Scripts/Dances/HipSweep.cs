750e78c17d8407c11ab10bc003339d9bbbdc2245
Wooki <wooki1@op.pl>
2018-07-21 17:50:28 +0200

    Dab and Sweep walking

--- Assets/Scripts/Dances/HipSweep.cs
+++ Assets/Scripts/Dances/HipSweep.cs
@@ -3,11 +3,26 @@
 
 public class HipSweep : Dance
 {
+    public const float THRUST_COOLDOWN = 1.0f;
+    public const float FORCE_IMPULSE = 150.0f;
+
+    private float cooldown = 0.0f;
+
     public override void StartDancing()
     {
+        cooldown = THRUST_COOLDOWN;
     }
 
     public override void Perform(GameObject character)
     {
+        if(cooldown > 0.0f)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else if (character.GetComponent<CharacterManager>().MovementDirection.magnitude > 0.0f)
+        {
+            character.GetComponent<Rigidbody>().AddForce(character.GetComponent<CharacterManager>().MovementDirection * FORCE_IMPULSE);
+            cooldown = THRUST_COOLDOWN;
+        }
     }
 }

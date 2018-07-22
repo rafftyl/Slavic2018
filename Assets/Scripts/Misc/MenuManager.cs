ce2c596e2e271f358f305789038960a6614232fd
Wooki <wooki1@op.pl>
2018-07-22 10:47:26 +0200

    Main menu

---
+++ Assets/Scripts/Misc/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject howToPlay;
+
+    public void Start()
+    {
+        mainMenu.active = true;
+        howToPlay.active = false;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void HowToPlay()
+    {
+        mainMenu.active = false;
+        howToPlay.active = true;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        mainMenu.active = true;
+        howToPlay.active = false;
+    }
+}

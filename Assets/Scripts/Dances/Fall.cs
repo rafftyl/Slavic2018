a27c88abebc4042597b4db4b310f09d4f742f1f4
Wooki <wooki1@op.pl>
2018-07-22 02:02:35 +0200

    Fall fix

--- Assets/Scripts/Dances/Fall.cs
+++ Assets/Scripts/Dances/Fall.cs
@@ -9,7 +9,7 @@ public class Fall : Dance
 
     public override string Name => "Fall";
 
-    public Fall() : base(new HashSet<int> { 0, 1, 2, 3 }, 1)
+    public Fall() : base(new HashSet<int> { 0, 1, 2, 3 }, 4)
     {
 
     }
